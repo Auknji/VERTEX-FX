31d90d806e415b6aa26cd9266029b1c5bd00340a
Auknji <124204197+Auknji@users.noreply.github.com>
2024-07-13 01:20:02 +0500

    .

--- utils/checkPrice.js
+++
@@ -1,8 +0,0 @@
-import getStockPrice from "./getStockPrice.js";
-
-const checkPrice = async (symbol) => {
-  const data = await getStockPrice(symbol);
-  return data.chart.result[0].meta.regularMarketPrice;
-};
-
-export default checkPrice;
--- utils/isBeforeGivenDay.js
+++
@@ -1,8 +0,0 @@
-import moment from "moment";
-
-export default function isBeforeGivenDay(next5DaysDate) {
-  const currentDate = moment();
-  const futureDate = moment(next5DaysDate);
-
-  return currentDate.isBefore(futureDate, "day");
-}
